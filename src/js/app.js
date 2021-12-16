8decad36297b553b7560d94644c666ea54d56c32
Liudmila Melezhikova <melezhikova@yandex.ru>
2021-12-16 23:16:48 +0500

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 // TODO: write your code here
 import { ajax } from 'rxjs/ajax';
+import { interval } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import Message from './Message';
 
 let newMessages = [];
@@ -7,21 +9,36 @@ let lastTimestamp = null;
 const url = 'http://localhost:7070/?method=messages/unread';
 let subscription;
 
-setInterval(() => {
-  const urlWithTime = `${url}&time=${lastTimestamp}`;
-  const data$ = ajax.getJSON(urlWithTime);
-  subscription = data$.subscribe(
+const data$ = interval(5000).pipe(
+  switchMap(ajax.getJSON(`${url}&time=${lastTimestamp}`)));
+data$.subscribe(
     (res) => {
-      console.log(res);
-      newMessages = res.messages;
-      lastTimestamp = res.timestamp;
-      newMessages.forEach((item) => {
-        const msg = new Message(item);
-        msg.addMessageToDOM();
-      });
-    },
-    (err) => console.error(err),
-  );
-}, 5000);
+            console.log(res);
+            newMessages = res.messages;
+            lastTimestamp = res.timestamp;
+            newMessages.forEach((item) => {
+              const msg = new Message(item);
+              msg.addMessageToDOM();
+            });
+          },
+          (err) => console.error(err),
+  )
+
+// setInterval(() => {
+//   const urlWithTime = `${url}&time=${lastTimestamp}`;
+//   const data$ = ajax.getJSON(urlWithTime);
+//   subscription = data$.subscribe(
+//     (res) => {
+//       console.log(res);
+//       newMessages = res.messages;
+//       lastTimestamp = res.timestamp;
+//       newMessages.forEach((item) => {
+//         const msg = new Message(item);
+//         msg.addMessageToDOM();
+//       });
+//     },
+//     (err) => console.error(err),
+//   );
+// }, 5000);
 
 setTimeout(() => subscription.unsubscribe(), 1000000);
